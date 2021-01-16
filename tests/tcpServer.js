fa84d649e08b0af9494b73eeb4ec2c6a3beb8f41
e.deiana <e.deiana@tripuls.de>
2021-01-16 18:21:45 +0100

    add animation

--- tests/tcpServer.js
+++ tests/tcpServer.js
@@ -11,7 +11,9 @@ const Player = require('../play');
 
 const server = net.createServer(function onConnectedSocket(client) {
     console.log('Connected client from', client.remoteAddress);
-    return Player('./animations/beavis_butthead.vt',
+    client.setEncoding('ascii');
+
+    const p = Player('./animations/snowing.vt',
         {clearBefore: false}) //need to deactivate default vt100's behaviour
         .on('data', function (data) {
             client.write(data); //send animation-data
@@ -19,7 +21,13 @@ const server = net.createServer(function onConnectedSocket(client) {
         .on('end', function () {
             client.write('\n\n\n\u001b[1000DHosted by @gr3p'); // send bye-message at begin of line
             client.destroy(); //destroy client-connection after animation is done
-        })
+        });
+
+    //handling error of disconnected clients
+    client.on('error', function (error) {
+        console.log('Error for', client.remoteAddress, error.message);
+        p.destroy(); //destroy vt100-stream
+    });
 });
 
 server.listen(23, '0.0.0.0', function () {
