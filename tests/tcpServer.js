b20eed72a70539266933bb82b141ab01b6dfa08e
e.deiana <e.deiana@tripuls.de>
2021-01-16 18:38:42 +0100

    using blinkyes for tcpServer example

--- tests/tcpServer.js
+++ tests/tcpServer.js
@@ -11,13 +11,13 @@ const Player = require('../play');
 
 const server = net.createServer(function onConnectedSocket(client) {
     console.log('Connected client from', client.remoteAddress);
-    const p = Player('./animations/snowing.vt',
+    const p = Player('./animations/blinkeyes.vt',
         {clearBefore: false}) //need to deactivate default vt100's behaviour
         .on('data', function (data) {
             client.write(data); //send animation-data
         })
         .on('end', function () {
-            client.write('\n\n\n\u001b[1000DHosted by @gr3p'); // send bye-message at begin of line
+            client.write('\n\n\n\u001b[1000D\n\nHosted by @gr3p'); // send bye-message at begin of line
             client.destroy(); //destroy client-connection after animation is done
         });
 
